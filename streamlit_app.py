75c0f959c0c6165ec3c84c4aef92364c2827717b
evierosejo <84474016+evierosejo@users.noreply.github.com>
2021-12-17 09:35:29 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,10 @@ The U.S. Supreme Court was created with the intention of impartiality, a
 branch of government that tamped down politics and human biases and dedicated itself to 
 interpretating the Constitution. However, is the Supreme Court truly nonpartisan?
 In a time when the decision of Roe v. Wade may be overturned, this question rings even louder. 
+
+## Party Politics
+
+The graph below shows how many judge appointments were made under presidents since this country's founding.
 ''')
 
 justices = pd.read_csv('/Users/evelynjohnson/Desktop/METIS/Engineering/project/justices.csv')
@@ -42,7 +46,31 @@ fig = px.bar(pres_appointments,
             )
 
 st.plotly_chart(fig, use_container_width=True)
-st.text("The graph above shows how many judge appointments were made under presidents since this country's founding.")
+
+opinions = pd.read_csv('/Users/evelynjohnson/Desktop/METIS/Engineering/project/opinions.csv')
+def get_years(df):
+    years = sorted(opinions.year_delivered.unique().tolist())
+
+    cases_per_year = pd.DataFrame(columns=['Year', 'Number of Cases'])
+    idx=0
+    for year in years:
+        count = opinions.Case.where(opinions.year_delivered == year).count()
+        cases_per_year.loc[idx] = [year, count]
+        idx += 1
+    
+    return cases_per_year
+
+amount_of_cases = get_years(opinions)
+
+st.write(
+'''
+## Opinions
+The graph below shows the number of opinions delivered per year. Interestingly, the amount of opinions delivered per year 
+has significantly decreased since 1968. 
+''')
+
+line_chart = px.line(amount_of_cases, x="Year", y="Number of Cases", title='Opinions Delivered Over Time')
+st.plotly_chart(line_chart, use_container_width=True)
 
 currentYear = datetime.now().year
 input_year= st.number_input(label='Enter a year between 1968 and {}'.format(currentYear), min_value=1968, max_value=currentYear)
